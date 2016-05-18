8ec9004c00d151d6d9e63357dfffa328310b8254
wangdan <1253864861@qq.com>
2016-05-18 12:56:06 +0800

    test.1

--- src/separators.js
+++ src/separators.js
@@ -1,24 +1,24 @@
 function thousands_separators(num) {
     var numArray = num.toString().split(".");
-    var integerArray = '';
+    var integer = '';
     var integerSplited =numArray[0].split("");
 
     for(var i = 0; i<integerSplited.length; i++)
     {
-        integerArray+=parseFloat(integerSplited[i]);
+        integer+=parseFloat(integerSplited[i]);
         if(Math.abs(integerSplited.length-1-i)%3 === 0 && integerSplited.length-1-i !== 0)
         {
-            integerArray+=',';
+            integer+=',';
         }
     }
 
     if(numArray[1])
     {
 
-        return integerArray + '.' + numArray[1];
+        return integer + '.' + numArray[1];
     }else{
 
-        return integerArray;
+        return integer;
     }
 }
 module.exports = thousands_separators;
